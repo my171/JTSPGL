a113135c5fc645c4bb2ed7e6bae17ea025a7959c
fslkgg <1739080506@qq.com>
2025-07-10 16:10:10 +0800

    仓库操作api更新

---
+++ front/JTSPGL-web/src/components/PopupStore.vue
@@ -0,0 +1,34 @@
+<!--PopupStore.vue-->
+<template>
+    <div class="popup-panel" :class="{ show: isVisible }">
+      <button class="btn btn-sm btn-outline-secondary mb-3" @click="close">关闭</button>
+      <h5>商店：{{ storeName }}</h5>
+      <p>这里可以显示商店相关信息，例如商品列表、销售记录等</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const isVisible = ref(false);
+  const storeName = ref('');
+  
+  const emit = defineEmits(['close']);
+  
+  const show = (name) => {
+    storeName.value = name;
+    isVisible.value = true;
+  };
+  
+  const close = () => {
+    isVisible.value = false;
+    emit('close');
+  };
+  
+  defineExpose({ show });
+  </script>
+  
+  <style scoped>
+  @import './popup-style.css';
+  </style>
+  
