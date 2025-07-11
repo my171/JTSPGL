55a0ed70749e6797531b8cff5b732a6586f20e7e
fslkgg <1739080506@qq.com>
2025-07-11 10:29:31 +0800

    PopupStore设计更新

--- front/JTSPGL-web/src/components/PopupStore.vue
+++ front/JTSPGL-web/src/components/PopupStore.vue
@@ -1,4 +1,3 @@
-<!--PopupStore.vue-->
 <template>
   <div class="popup-panel" :class="{ show: isVisible }">
     <button class="btn btn-sm btn-outline-secondary mb-3" @click="close">
@@ -16,9 +15,7 @@
         v-model="queryInput"
         placeholder="输入商品ID或名称"
       />
-      <button class="btn btn-primary" @click="queryProduct">
-        查询信息
-      </button>
+      <button class="btn btn-primary" @click="queryProduct">查询信息</button>
     </div>
     <div v-if="productResult">
       <pre>{{ productResult }}</pre>
@@ -32,13 +29,9 @@
         v-model="transferProduct"
         placeholder="商品ID"
       />
-      <input
-        class="form-control"
-        v-model="transferQty"
-        placeholder="数量"
-      />
-      <select class="form-select" v-model="selectedWarehouse">
-        <option disabled value="">选择仓库</option>
+      <input class="form-control" v-model="transferQty" placeholder="数量" />
+      <select class="form-select" v-model="selectedWarehouseId">
+        <option disabled value="">选择调出仓库</option>
         <option v-for="wh in warehouseList" :key="wh.id" :value="wh.id">
           {{ wh.name }}
         </option>
@@ -49,63 +42,61 @@
     <!-- 卖出商品 -->
     <h5 class="mt-4">卖出商品</h5>
     <div class="input-group mb-2">
-      <input
-        class="form-control"
-        v-model="sellProduct"
-        placeholder="商品ID"
-      />
-      <input
-        class="form-control"
-        v-model="sellQty"
-        placeholder="数量"
-      />
+      <input class="form-control" v-model="sellProduct" placeholder="商品ID" />
+      <input class="form-control" v-model="sellQty" placeholder="数量" />
       <button class="btn btn-success" @click="sell">卖出</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref } from "vue";
+import axios from "axios";
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(["close"]);
 const isVisible = ref(false);
-const storeName = ref('');
+const storeId = ref("");
+const storeName = ref("");
 
-const queryInput = ref('');
-const productResult = ref('');
+const queryInput = ref("");
+const productResult = ref("");
 
-const transferProduct = ref('');
-const transferQty = ref('');
-const selectedWarehouse = ref('');
+const transferProduct = ref("");
+const transferQty = ref("");
+const selectedWarehouseId = ref("");
 const warehouseList = ref([]);
 
-const sellProduct = ref('');
-const sellQty = ref('');
+const sellProduct = ref("");
+const sellQty = ref([]);
 
-// 打开弹窗
-const show = async (name) => {
+// 打开弹窗（传入商店 id 和 name）
+const show = async (id, name) => {
+  storeId.value = id;
   storeName.value = name;
   isVisible.value = true;
 
-  // 加载仓库列表供选择（调货用）
-  try {
-    const res = await axios.get('http://localhost:5000/api/warehouses');
-    warehouseList.value = res.data;
-  } catch (err) {
-    console.error('加载仓库列表失败', err);
-  }
+  // 加载仓库列表
+  warehouseList.value = [
+    { id: "WH001", name: "华北中心仓" },
+    { id: "WH002", name: "华东智能仓" },
+    { id: "WH003", name: "华南枢纽仓" },
+    { id: "WH004", name: "西南分拨中心" },
+    { id: "WH005", name: "东北冷链仓" },
+  ];
 };
 
-// 查询商品信息和销量
+// 查询商品信息
 const queryProduct = async () => {
   try {
-    const res = await axios.get('http://localhost:5000/api/store/product/full', {
-      params: {
-        store: storeName.value,
-        query: queryInput.value
+    const res = await axios.get(
+      "http://localhost:5000/api/store/product/full",
+      {
+        params: {
+          store_id: storeId.value,
+          query: queryInput.value,
+        },
       }
-    });
+    );
     productResult.value = JSON.stringify(res.data, null, 2);
   } catch (err) {
     productResult.value = `查询失败：${err.message}`;
@@ -115,13 +106,17 @@ const queryProduct = async () => {
 // 从仓库调货
 const transferIn = async () => {
   try {
-    await axios.post('http://localhost:5000/api/store/transfer-in', {
-      store: storeName.value,
+    const fromWarehouseName =
+      warehouseList.value.find((w) => w.id === selectedWarehouseId.value)
+        ?.name || "";
+    await axios.post("http://localhost:5000/api/store/transfer-in", {
+      store_id: storeId.value,
       product: transferProduct.value,
       quantity: Number(transferQty.value),
-      fromWarehouse: selectedWarehouse.value
+      from_warehouse_id: selectedWarehouseId.value,
+      from_warehouse_name: fromWarehouseName,
     });
-    alert('调货成功');
+    alert("调货成功");
   } catch (err) {
     alert(`调货失败：${err.message}`);
   }
@@ -130,21 +125,20 @@ const transferIn = async () => {
 // 卖出商品
 const sell = async () => {
   try {
-    await axios.post('http://localhost:5000/api/store/sell', {
-      store: storeName.value,
+    await axios.post("http://localhost:5000/api/store/sell", {
+      store_id: storeId.value,
       product: sellProduct.value,
-      quantity: Number(sellQty.value)
+      quantity: Number(sellQty.value),
     });
-    alert('卖出成功');
+    alert("卖出成功");
   } catch (err) {
     alert(`卖出失败：${err.message}`);
   }
 };
 
-// 关闭弹窗
 const close = () => {
   isVisible.value = false;
-  emit('close');
+  emit("close");
 };
 
 const relatedclose = () => {
@@ -155,5 +149,5 @@ defineExpose({ show, relatedclose });
 </script>
 
 <style scoped>
-@import './popup-style.css';
+@import "./popup-style.css";
 </style>
