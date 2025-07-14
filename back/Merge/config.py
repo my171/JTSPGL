553946c151deb74034ea635d2c562bf1fa92a82c
histok <histok@163.com>
2025-07-14 09:31:53 +0800

    尝试多次输出

--- back/Merge/config.py
+++ back/Merge/config.py
@@ -4,8 +4,8 @@ class Config:
     DB_HOST = '127.0.0.1'
     DB_PORT = '5432'
     DB_NAME = 'postgres'
-    DB_USER = 'u1'
-    DB_PASSWORD = 'ab12AB!@'
+    DB_USER = 'postgres'
+    DB_PASSWORD = 'DSds178200++'
     client_encoding='utf8'
     
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?client_encoding=utf8'
