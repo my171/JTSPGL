5ae93b0bd3bfb114968a76c1b6b864d851564e88
Kois <122266302+Koissssss@users.noreply.github.com>
2025-07-15 04:47:29 +0800

    8 RAG是真测不来，歇逼

--- back/Merge/config.py
+++ back/Merge/config.py
@@ -11,3 +11,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?client_encoding=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key-here' 
+
+
+    RAG_OPENAI_API_KEY = "sk-ubjkrzodjlihepttrgdmmqsxaulmoktrzvmvzzwpkaftmtcn"
+    RAG_OPENAI_API_URL = "https://api.siliconflow.cn/v1"
+    RAG_MODEL_NAME = "Qwen/Qwen2.5-72B-Instruct"
+    RAG_EMBEDDING_MODEL = "Qwen/Qwen3-Embedding-4B"
+    RAG_PDF_DIR = "./API_TextToText/knowledge_pdfs"
